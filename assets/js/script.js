25f1722b45238be7687b095dd673c594db5c3100
mcgreevyjim66 <mcgreevyjim66@yahoo.com>
2023-07-26 15:44:57 -0400

    updating html, css, and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
+// declare element objects on html for processing
+var timerElement = document.querySelector("#timer-span");
+var resultBarElement = document.querySelector("#result-bar");
 var startButton = document.querySelector(".start-button");
+var resetButton = document.querySelector(".reset-button");
 
 var questiontext = document.querySelector("#question-text");
 var answer1BtnObj = document.querySelector("#answerBtn1");
@@ -10,20 +10,38 @@ var answer2BtnObj = document.querySelector("#answerBtn2");
 var answer3BtnObj = document.querySelector("#answerBtn3");
 var answer4BtnObj = document.querySelector("#answerBtn4");
 
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
+var resultsForm = document.querySelector("#results-form");
+var resultsList = document.querySelector("#results-list");
+var resultsCountSpan = document.querySelector("#results-count");
+
+
+
+  // Get the dialog element
+  const resultsDialog = document.getElementById("resultsDialog");
+
+  // Get the open dialog button
+  const openDialogBtn = document.getElementById("openDialogBtn");
+  
+  // Get the close dialog button
+  const closeDialogBtn = document.getElementById("closeDialogBtn");
+  
+  // Get the apply criteria button
+  const applyDialogBtn = document.getElementById("applyDialogBtn");
+
+  // Get generateForm element
+  const myForm = document.getElementById("generateForm");
+
+  var userinitials = document.getElementById("userinitials");
+
+//declare global variables used in processing
+
 var timer;
 var timerCount;
+var currentquestionnumber = 0;
+var totalscore = 0;
+var userinitials = ""
 
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
 
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
 // define quiz object
 const quizQuestion1obj = {
   question: "Who was first president of United States?",
@@ -43,75 +61,338 @@ const quizQuestion2obj = {
   correctAnswer: "65 mph",
   gotCorrect: false,
 };
-// array og question objects
-var questions = [quizQuestion1obj, quizQuestion2obj];
+const quizQuestion3obj = {
+  question: "How many planets are there in the solar system?",
+  answer1: "7",
+  answer2: "8",
+  answer3: "9",
+  answer4: "10",
+  correctAnswer: "8",
+  gotCorrect: false,
+};
+const quizQuestion4obj = {
+  question: "What is the largest mammal on Earth?",
+  answer1: "Hippo",
+  answer2: "Elephant",
+  answer3: "Giraffe",
+  answer4: "Blue Whale",
+  correctAnswer: "Blue Whale",
+  gotCorrect: false,
+};
+const quizQuestion5obj = {
+  question: "What is the acceleration of gravity?",
+  answer1: "10 m/s",
+  answer2: "7 ms",
+  answer3: "9.8 m/s",
+  answer4: "5 m/s",
+  correctAnswer: "9.8 m/s",
+  gotCorrect: false,
+};
+// array of question objects
+var ArrayofQuestions = [quizQuestion1obj, quizQuestion2obj, quizQuestion3obj, quizQuestion4obj, quizQuestion5obj];
+var numQuestions = ArrayofQuestions.length;
+
+//declare score object $ array
+var ScoreObj = {
+  initials: "",
+  quizscore: 0,
+};
+var ArrayofScores =[];
+
 
 
 // The init function is called when the page loads 
 function init() {
-  getWins();
-  getlosses();
-}
 
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isDone = false;
+    // Get stored todos from localStorage
+    var storedscores = JSON.parse(localStorage.getItem("arrayofscores"));
+   // If were retrieved from localStorage, update the todos array to it
+  if (storedscores !== null) {
+    ArrayofScores = storedscores;
+  } else {
+    ArrayofScores =[];
+  }
+  renderScores();
+
+  startButton.disabled = false;
+  startButton.style.display = "inline";
+  resetButton.disabled = false;
+  isDone = true;
+  currentquestionnumber = 0;
   timerCount = 50;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  //renderBlanks()
-  //startTimer()
-  startQuiz()
-}
-function startQuiz(){
-  var numQuestions = questions.length;
 
-  for (i = 0; i < numQuestions; i++){
-    displayQuestion();
-    answeredCorrectly = getAnswer();
+  timerElement.textContent = timerCount;
+  questiontext.textContent = "Press Start button to begin new quiz."
+  //answer1BtnObj.textContent = "";
+  //answer2BtnObj.textContent = "Answer # 2";
+  //answer3BtnObj.textContent = "Answer # 3";
+  //answer4BtnObj.textContent = "Answer # 4";
+ 
+  answer1BtnObj.style.display = "none";
+  answer2BtnObj.style.display = "none";
+  answer3BtnObj.style.display = "none";
+  answer4BtnObj.style.display = "none";
 
+  resultBarElement.textContent = "";
+
+  }
+// store latest score in local storage
+  function StoreScore() {
+    // Stringify and set key in localStorage scores array
+    ScoreObj.initials = userinitials.value;
+    ScoreObj.quizscore = totalscore;
+    ArrayofScores.push(ScoreObj);
+    localStorage.setItem("arrayofscores", JSON.stringify(ArrayofScores));
+  }
+// start a new quiz
+  function startQuiz(){
+    var numQuestions = ArrayofQuestions.length;
+    currentquestionnumber = 0;
+    isDone = false;
+    timerCount = 50;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    resetButton.disabled = true;
+    startButton.style.display = "none";
+    answer1BtnObj.style.display = "inline"; 
+    answer2BtnObj.style.display = "inline"; 
+    answer3BtnObj.style.display = "inline"; 
+    answer4BtnObj.style.display = "inline"; 
+
+      startTimer();
+      displayQuestion();
+      listenforAnswer();
+
+  }
+  //stop current running quiz
+  function stopQuiz(){
+
+    answer1BtnObj.removeEventListener("click", answer1Clicked);
+    answer2BtnObj.removeEventListener("click", answer2Clicked);
+    answer3BtnObj.removeEventListener("click", answer3Clicked);
+    answer4BtnObj.removeEventListener("click", answer4Clicked);
+
+    CalculateScore();
+
+    //StoreScore();
+    openDialog();
+
+    startButton.disabled = false;
+    startButton.style.display = "inline";
+    resetButton.disabled = false;
+    isDone = true;
+
+    questiontext.textContent = "Press Start button to begin new quiz."
+  
+    answer1BtnObj.style.display = "none";
+    answer2BtnObj.style.display = "none";
+    answer3BtnObj.style.display = "none";
+    answer4BtnObj.style.display = "none";
+    resultBarElement.textContent = "";
+     
+
+  }
+  //reset quiz scores
+  function resetQuiz(){
+    localStorage.clear();
+    init();
+    startButton.disabled = false;
+    resetButton.disabled = false;
+    isDone = true;
+ 
+  }
+  //calculate score of last quiz
+  function CalculateScore(){
+    totalscore =0;
+    for (i=0; (i + 1) <=ArrayofQuestions.length; i++)
+    { 
+      if (ArrayofQuestions[i].gotCorrect)
+          totalscore++;
     }
-}
+  }
+  //continue with next question in quiz
+  function continueQuiz(){
 
+    currentquestionnumber++;
+
+
+      displayQuestion();
+      listenforAnswer();
+
+  }
+
+  
+// display the current question on the page
 function displayQuestion(){
-  currentQuestion = questions[i];
+  currentQuestion = ArrayofQuestions[currentquestionnumber];
   questiontext.textContent = currentQuestion.question;
   answer1BtnObj.textContent = currentQuestion.answer1;
   answer2BtnObj.textContent = currentQuestion.answer2;
   answer3BtnObj.textContent = currentQuestion.answer3;
   answer4BtnObj.textContent = currentQuestion.answer4;
 }
-
-function getAnswer(){
+//start event listeners for quiz answer buttons
+function listenforAnswer(){
   var answerClicked = false;
   answer1BtnObj.addEventListener("click", answer1Clicked);
- // answer2BtnObj.addEventListener("click", answer2Clicked);
- // answer3BtnObj.addEventListener("click", answer3Clicked);
- // answer4BtnObj.addEventListener("click", answer4Clicked);
-
-  //while (!answerClicked){
-
-  //}
+  answer2BtnObj.addEventListener("click", answer2Clicked);
+  answer3BtnObj.addEventListener("click", answer3Clicked);
+  answer4BtnObj.addEventListener("click", answer4Clicked);
 
 }
+
+//proces quiz answer buttons
 function answer1Clicked(){
 
-}
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
+if (currentQuestion.correctAnswer == currentQuestion.answer1){
+  ArrayofQuestions[currentquestionnumber].gotCorrect = true;
+  resultBarElement.textContent = "Answer Correct!";
+
+} else {
+  ArrayofQuestions[currentquestionnumber].gotCorrect = false;
+  resultBarElement.textContent = "Answer Incorrect! 15 seconds deducted from Timer!";
+  timerCount = timerCount - 15;
+  if (timerCount <= 0){timerCount = 1} 
 }
 
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
+if ((currentquestionnumber + 1) == ArrayofQuestions.length) {
+    stopQuiz()
+  } else {
+    continueQuiz();
+  }
+
 }
+function answer2Clicked(){
+
+  if (currentQuestion.correctAnswer == currentQuestion.answer2){
+    ArrayofQuestions[currentquestionnumber].gotCorrect = true;
+    resultBarElement.textContent = "Answer Correct!";
+  } else {
+    ArrayofQuestions[currentquestionnumber].gotCorrect = false;
+    resultBarElement.textContent = "Answer Incorrect! 15 seconds deducted from Timer!";
+    timerCount = timerCount - 15;
+    if (timerCount <= 0){timerCount = 1} 
+  }
+  
+  if ((currentquestionnumber + 1) == ArrayofQuestions.length) {
+      stopQuiz()
+    } else {
+      continueQuiz();
+    }
+  
+  }
+ 
+  function answer3Clicked(){
+
+    if (currentQuestion.correctAnswer == currentQuestion.answer3){
+      ArrayofQuestions[currentquestionnumber].gotCorrect = true;
+      resultBarElement.textContent = "Answer Correct!";
+    } else {
+      ArrayofQuestions[currentquestionnumber].gotCorrect = false;
+      resultBarElement.textContent = "Answer Incorrect! 15 seconds deducted from Timer!";
+      timerCount = timerCount - 15;
+      if (timerCount <= 0){timerCount = 1} 
+    }
+    
+    if ((currentquestionnumber + 1) == ArrayofQuestions.length) {
+        stopQuiz()
+      } else {
+        continueQuiz();
+      }
+    
+    }
+ 
+    function answer4Clicked(){
+
+      if (currentQuestion.correctAnswer == currentQuestion.answer4){
+        ArrayofQuestions[currentquestionnumber].gotCorrect = true;
+        resultBarElement.textContent = "Answer Correct!";
+      
+      } else {
+        ArrayofQuestions[currentquestionnumber].gotCorrect = false;
+        resultBarElement.textContent = "Answer Incorrect! 15 seconds deducted from Timer!";
+        timerCount = timerCount - 15;
+        if (timerCount <= 0){timerCount = 1} 
+      }
+      
+      if ((currentquestionnumber + 1) == ArrayofQuestions.length) {
+          stopQuiz()
+        } else {
+          continueQuiz();
+        }
+      
+      }
+
+
+
+// code for score saving dialog processing
+
+
+        // Function to open the dialog
+        function openDialog() {
+          resultsDialog.showModal();
+        }
+
+
+        // Function to close the dialog
+        function closeDialog() {
+          
+          myForm.reset();
+          resultsDialog.close();
+          init();
+        
+        }
+
+        function applyDialog() {
+       
+            // Get initials from dialog
+            userinitials = document.getElementById("userinitials");
+            //console.log("user initials " + userinitials.value)
+        
+          if (userinitials.value.length > 0)
+          {
+              StoreScore();
+         
+          } else {
+                  window.alert("You did not enter any user initials.");
+                  return; 
+                 }
+        
+                // Close the dialog
+          closeDialog();
+          renderScores();
+        }
+         
+        
+        
+// end code for dialog processing
+
+// code for past scores processing
+
+// The following function renders items in a todo list as <li> elements
+function renderScores() {
+  // Clear past scores results lsit
+  resultsList.innerHTML = "";
+  
+  // Render a new li for each score
+  for (var i = 0; i < ArrayofScores.length; i++) {
+    var initresult = ArrayofScores[i].initials;
+    var quizresult = ArrayofScores[i].quizscore;
+
+    var li = document.createElement("li");
+    li.textContent = "User: " + initresult + " Scored: " + quizresult;
+    li.setAttribute("data-index", i);
+
+
+    resultsList.appendChild(li);
+  }
+}
+
+
+
+// end code scors processing
+
+
 
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
@@ -120,133 +401,34 @@ function startTimer() {
     timerCount--;
     timerElement.textContent = timerCount;
     if (timerCount >= 0) {
-      // Tests if win condition is met
+      // Tests if isdone condition is met
       if (isDone && timerCount > 0) {
         // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
+          clearInterval(timer);
+          stopQuiz();
       }
     }
     // Tests if time has run out
-    if (timerCount === 0) {
+    if (timerCount <= 0) {
       // Clears interval
-      clearInterval(timer);
-      loseGame();
+       clearInterval(timer);
+       timerCount = 0;
+      stopQuiz();
     }
   }, 1000);
 }
 
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
-
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isDone = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
 
 // Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startQuiz);
+
+resetButton.addEventListener("click", resetQuiz);
+
+// Event listeners
+closeDialogBtn.addEventListener("click", closeDialog);
+applyDialogBtn.addEventListener("click", applyDialog);
 
 // Calls init() so that it fires when page opened
 init();
 
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
 
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
